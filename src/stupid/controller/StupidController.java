5601096485ddc278eb6945fc6db3cf9c25f20be0
ConnorOLeary <themidlvlboss@gmail.com>
2017-09-05 09:38:20 -0600

    Initial commit

---
+++ src/stupid/controller/StupidController.java
@@ -0,0 +1,8 @@
+package stupid.controller;
+
+public class StupidController {
+
+	public void start() {
+		System.out.println("The holocaust and 9/11, that shits funny, 24/7");
+	}
+}
---
+++ src/stupid/controller/StupidRunner.java
@@ -0,0 +1,8 @@
+package stupid.controller;
+
+public class StupidRunner {
+	public static void main(String[]args) {
+		StupidController firstApp = new StupidController();
+		firstApp.start();
+	}
+}
